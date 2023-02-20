7181a10ef9700039aa1640713032c95a1ab717a5
Akash Kumar Singh <akashviratsingh018@gmail.com>
2023-02-20 13:29:39 +0530

    added: cart route

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
-const users = require("./users.route");
-const products = require("./products.route");
-const orders = require("./orders.route")
-const carts = require("./carts.route")
+ const users = require("./users.route");
+ const products = require("./products.route");
+ const orders = require("./orders.route")
+ const carts = require("./carts.route")
 
-module.exports = {users, products, orders, carts};
+ module.exports = {users, products, orders, carts};
