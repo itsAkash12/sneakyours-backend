2cb725cba6f9bb7c553aa460a1d6085bec3a95eb
Akash Kumar Singh <akashviratsingh018@gmail.com>
2023-02-21 18:42:47 +0530

    added: backend routes for wishlist

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
- const users = require("./users.route");
- const products = require("./products.route");
- const orders = require("./orders.route")
- const carts = require("./carts.route")
+const users = require("./users.route");
+const products = require("./products.route");
+const orders = require("./orders.route")
+const carts = require("./carts.route")
+const wishlists = require("./wishlist.route")
 
- module.exports = {users, products, orders, carts};
+module.exports = {users, products, orders, carts, wishlists};
